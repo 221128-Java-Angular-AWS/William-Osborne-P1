8afe527cc744a60ec115cc7ed2e8622236f08262
Will <weosborne90@gmail.com>
2022-12-02 14:05:34 -0500

    Added User object functionality and began building out menu functionality

--- Main.java
+++ Main.java
@@ -1,6 +1,12 @@
+
 public class Main {
     public static void main(String[] args){
         System.out.println("Starting here");
+        String loginStatus = UserLogin.login();
+        System.out.println(loginStatus);
+        if (loginStatus == "Login Successful"){
+            User user = new User("user", "admin");
+        }
     }
 
 }
--- UserLogin.java
+++ UserLogin.java
@@ -1,6 +1,25 @@
+import java.util.Scanner;
 public class UserLogin {
-    private String username;
-    private String password;
+    public static String login(){
+        String username = "user";
+        String password = "pass";
+
+        // maybe make this try with resources
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Please enter username: ");
+        String userInput = sc.nextLine();
+        System.out.println("Please enter password for " + userInput + " : ");
+        String passInput = sc.nextLine();
+        sc.close();
+
+        if (userInput.equals(username) && passInput.equals(password)){
+            return "Login successful.";
+        } else{
+            return "Invalid credentials.";
+        }
+    }
+
 
 
 }
